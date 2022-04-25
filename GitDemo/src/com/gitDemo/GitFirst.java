e948657ed1a8df403411d295752405c1e590b569
Chet1Patil <104189891+Chet1Patil@users.noreply.github.com>
2022-04-25 12:30:58 +0530

    Update GitFirst.java

--- GitDemo/src/com/gitDemo/GitFirst.java
+++ GitDemo/src/com/gitDemo/GitFirst.java
@@ -7,6 +7,12 @@ public class GitFirst
 		System.out.println("This is method 1");
 		
 	}
+	public void m2()
+	{
+		System.out.println("This is method 2");
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
