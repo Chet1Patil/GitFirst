a799342907f2475ccd1c7ed3276eee8778910a62
lenovo <lenovo@LAPTOP-9J0HJRM6>
2022-04-25 12:11:22 +0530

    m1 method added

--- GitDemo/src/com/gitDemo/GitFirst.java
+++ GitDemo/src/com/gitDemo/GitFirst.java
@@ -1,6 +1,12 @@
 package com.gitDemo;
 
-public class GitFirst {
+public class GitFirst 
+{
+	public void m1()
+	{
+		System.out.println("This is method 1");
+		
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
